04750856b3496cc75603f43c2781352b1106e94e
Biyonda Kurniawan <biyondakurniawan@gmail.com>
2021-09-23 01:40:38 +0700

    splash dan navbottombar

---
+++ app/src/main/java/com/example/viewabsensi/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.viewabsensi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent home = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        }, 3000);
+
+    }
+}
