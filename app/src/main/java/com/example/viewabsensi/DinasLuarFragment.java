9e2651771bb6152cc11ae18624022d8e65a0b78f
Biyonda Kurniawan <biyondakurniawan@gmail.com>
2021-09-24 02:31:46 +0700

    tabbed

---
+++ app/src/main/java/com/example/viewabsensi/DinasLuarFragment.java
@@ -0,0 +1,18 @@
+package com.example.viewabsensi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DinasLuarFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_dinas_luar, container, false);
+    }
+}
