a4b0ef041f25770b3d7beefe9a4d3366a11aac4d
Biyonda Kurniawan <biyondakurniawan@gmail.com>
2021-09-24 00:41:34 +0700

    dashboard

--- app/src/main/java/com/example/viewabsensi/BerandaFragment.java
+++ app/src/main/java/com/example/viewabsensi/BerandaFragment.java
@@ -3,40 +3,29 @@ package com.example.viewabsensi;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BerandaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BerandaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    Handler handler = new Handler();
 
     public BerandaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BerandaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BerandaFragment newInstance(String param1, String param2) {
         BerandaFragment fragment = new BerandaFragment();
         Bundle args = new Bundle();
@@ -59,6 +48,22 @@ public class BerandaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda, container, false);
+        View view = inflater.inflate(R.layout.fragment_beranda, container, false);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 1000);
+            }
+        });
+
+        return view;
     }
 }
