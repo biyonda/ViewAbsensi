0fb4e9461756199f4fceded34c580e24c7f3d37d
Biyonda Kurniawan / LV <biyondakurniawan@gmail.com>
2021-09-27 16:59:56 +0700

    absen

--- app/src/main/java/com/example/viewabsensi/DinasDalamFragment.java
+++ app/src/main/java/com/example/viewabsensi/DinasDalamFragment.java
@@ -8,17 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class DinasDalamFragment extends Fragment {
 
-    Button btn_pilih_absensi;
+    LinearLayout btn_pilih_absensi;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dinas_dalam, container, false);
 
-        btn_pilih_absensi = (Button) view.findViewById(R.id.btn_pilih_absensi);
+        btn_pilih_absensi = (LinearLayout) view.findViewById(R.id.btn_pilih_absensi);
 
         btn_pilih_absensi.setOnClickListener(new View.OnClickListener() {
             @Override
