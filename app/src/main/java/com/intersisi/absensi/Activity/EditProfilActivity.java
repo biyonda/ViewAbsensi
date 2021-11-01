f3e892bfe7dcd3221410e2d1ca67ebed0b433651
Biyonda Kurniawan / LV <biyondakurniawan@gmail.com>
2021-11-01 11:03:58 +0700

    kata2 presensi

--- app/src/main/java/com/intersisi/absensi/Activity/EditProfilActivity.java
+++ app/src/main/java/com/intersisi/absensi/Activity/EditProfilActivity.java
@@ -3,14 +3,55 @@ package com.intersisi.absensi.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.intersisi.absensi.Api.Api;
+import com.intersisi.absensi.Api.RetrofitClient;
 import com.intersisi.absensi.R;
+import com.intersisi.absensi.Response.BaseResponse;
+import com.intersisi.absensi.Session.Session;
+import com.intersisi.absensi.Table.JadwalHariIni;
+
+import retrofit2.Call;
 
 public class EditProfilActivity extends AppCompatActivity {
+
+    EditText nama_pengguna, no_pengguna, password_pengguna;
+    Spinner shift_spinner;
+    Button btn_simpan;
+
+    Session session;
+    Api api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profil);
+
+        nama_pengguna = findViewById(R.id.nama_pengguna);
+        no_pengguna = findViewById(R.id.no_pengguna);
+        password_pengguna = findViewById(R.id.password_pengguna);
+
+        ArrayAdapter<CharSequence> kelshift_spinner = ArrayAdapter.createFromResource(this, R.array.shift_array, android.R.layout.simple_spinner_item);
+        kelshift_spinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        shift_spinner.setAdapter(kelshift_spinner);
+
+        session = new Session(this);
+        api = RetrofitClient.createServiceWithAuth(Api.class, session.getToken());
+
+        nama_pengguna.setText(session.getNama());
+        no_pengguna.setText(session.getNip());
+
+        btn_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/intersisi/absensi/Activity/ProfilFragment.java
+++ app/src/main/java/com/intersisi/absensi/Activity/ProfilFragment.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.intersisi.absensi.Api.Api;
 import com.intersisi.absensi.Api.RetrofitClient;
+import com.intersisi.absensi.Helpers.ApiError;
+import com.intersisi.absensi.Helpers.ErrorUtils;
 import com.intersisi.absensi.R;
 import com.intersisi.absensi.Response.BaseResponse;
 import com.intersisi.absensi.Session.Session;
+import com.intersisi.absensi.Table.JadwalHariIni;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,10 +32,13 @@ import retrofit2.Response;
 
 public class ProfilFragment extends Fragment {
 
-    RelativeLayout btn_logout;
+    RelativeLayout btn_logout, btn_edit;
+    TextView nama_pengguna, no_pegawai, jabatan_pengguna, shift;
+    ImageView foto_pengguna;
 
     Session session;
-//    Api api;
+    Api api;
+    Call<BaseResponse<JadwalHariIni>> getJadwalHariIni;
 //    Call<BaseResponse> logout;
 
     @Override
@@ -34,7 +47,25 @@ public class ProfilFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profil, container, false);
 
         session = new Session(getContext());
-//        api = RetrofitClient.createServiceWithAuth(Api.class, session.getToken());
+        api = RetrofitClient.createServiceWithAuth(Api.class, session.getToken());
+
+        nama_pengguna = view.findViewById(R.id.nama_pengguna);
+        nama_pengguna.setText(session.getNama());
+        no_pegawai = view.findViewById(R.id.no_pegawai);
+        no_pegawai.setText(session.getNip());
+        jabatan_pengguna = view.findViewById(R.id.jabatan_pengguna);
+        jabatan_pengguna.setText(session.getNamaBagian());
+        shift = view.findViewById(R.id.shift);
+
+        jadwalHariIni(session.getNip());
+
+        btn_edit = view.findViewById(R.id.btn_edit);
+        btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), EditProfilActivity.class));
+            }
+        });
 
         btn_logout = view.findViewById(R.id.btn_logout);
         btn_logout.setOnClickListener(new View.OnClickListener() {
@@ -47,5 +78,36 @@ public class ProfilFragment extends Fragment {
         });
 
         return view;
+    }
+
+    public void jadwalHariIni(String nip) {
+        getJadwalHariIni = api.getJadwalHariIni(session.getNip());
+        getJadwalHariIni.enqueue(new Callback<BaseResponse<JadwalHariIni>>() {
+            @Override
+            public void onResponse(Call<BaseResponse<JadwalHariIni>> call, Response<BaseResponse<JadwalHariIni>> response) {
+                if (response.isSuccessful()) {
+                    ArrayList<String> jadwalku = new ArrayList<>();
+                    jadwalku.clear();
+
+                    for (int i = 0; i < response.body().getData().size(); i++) {
+                        jadwalku.add(response.body().getData().get(i).getNama());
+                    }
+
+                    Collections.reverse(jadwalku);
+                    shift.setText("Kelompok Shift : "+ TextUtils.join(", ", jadwalku));
+
+                } else {
+                    shift.setText("Kelompok Shift : -");
+                    ApiError apiError = ErrorUtils.parseError(response);
+                    Toast.makeText(getContext(), apiError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BaseResponse<JadwalHariIni>> call, Throwable t) {
+                shift.setText("Kelompok Shift : -");
+                Toast.makeText(getContext(), "Error "+t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
